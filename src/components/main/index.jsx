ab1f85b54a28ebd2ff0c962da6f20cb387e42283
InvokFung <44592591+InvokFung@users.noreply.github.com>
2024-02-20 02:17:58 +0800

    updated page

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -6,6 +6,14 @@ const Main = () => {
     const mainContainer = useRef();
     const fileInput = useRef();
     const resultField = useRef();
+
+    const depthMinVal = useRef();
+    const depthMaxVal = useRef();
+    const fxVal = useRef();
+    const fyVal = useRef();
+    const cxVal = useRef();
+    const cyVal = useRef();
+
     const [images, setImages] = useState([]);
     const [result, setResult] = useState(null);
     const [resultRetrieved, setResultRetrieved] = useState(false);
@@ -24,11 +32,33 @@ const Main = () => {
 
     const handleFormSubmit = (event) => {
         event.preventDefault();
+        
+        if (images.length === 0) {
+            return alert("No images were uploaded");
+        }
+
+        console.log(`Requesting ${images.length} images`)
 
         const formData = new FormData();
         images.forEach((image, index) => {
             formData.append('images', image, image.name);
         });
+
+        // Add extra parameters
+        const parameters = {
+            depthMin: depthMinVal.current.value,
+            depthMax: depthMaxVal.current.value,
+            fx: fxVal.current.value,
+            fy: fyVal.current.value,
+            cx: cxVal.current.value,
+            cy: cyVal.current.value
+        };
+
+        // Convert parameters to JSON string
+        const parametersJson = JSON.stringify(parameters);
+
+        // Append parameters to formData
+        formData.append('parameters', parametersJson);
 
         const userId = 1;
 
@@ -56,6 +86,8 @@ const Main = () => {
     const handleDrop = (event) => {
         event.preventDefault();
 
+        let newImages = [...images];  // Create a copy of the current images
+
         if (event.dataTransfer.items) {
             // Use DataTransferItemList interface to access the file(s)
             for (var i = 0; i < event.dataTransfer.items.length; i++) {
@@ -66,7 +98,7 @@ const Main = () => {
 
                     // Check if file type is jpg, jpeg or png
                     if (fileType === 'jpg' || fileType === 'jpeg' || fileType === 'png') {
-                        setImages(prevImages => [...prevImages, file]);
+                        newImages.push(file);
                     } else {
                         alert("Invalid file type. Please upload only jpg, jpeg or png files.")
                     }
@@ -79,12 +111,13 @@ const Main = () => {
 
                 // Check if file type is jpg, jpeg or png
                 if (fileType === 'jpg' || fileType === 'jpeg' || fileType === 'png') {
-                    setImages(prevImages => [...prevImages, event.dataTransfer.files[i]]);
+                    newImages.push(event.dataTransfer.files[i]);
                 } else {
                     alert("Invalid file type. Please upload only jpg, jpeg or png files.")
                 }
             }
         }
+        setImages(newImages);
     }
 
     return (
@@ -128,6 +161,50 @@ const Main = () => {
                 )}
             </div>
             <div className='right-content'>
+                <div className='rcs-parameter'>
+                    <div className='rp-header'>Parameters</div>
+                    <div className='rp-content'>
+                        <div className='rp-item'>
+                            <div>Object Depth</div>
+                            <div className='multiInput'>
+                                <div className='halfText'>
+                                    <label>min</label>
+                                    <input className='halfInput' type='number' step="0.01" ref={depthMinVal} defaultValue={0} />
+                                </div>
+                                <div className='halfText'>
+                                    <label>max</label>
+                                    <input className='halfInput' type='number' step="0.01" ref={depthMaxVal} defaultValue={0} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='rp-item'>
+                            <div>Camera focal length</div>
+                            <div className='multiInput'>
+                                <div className='halfText'>
+                                    <label>fx</label>
+                                    <input className='halfInput' type='number' step="0.01" ref={fxVal} defaultValue={0} />
+                                </div>
+                                <div className='halfText'>
+                                    <label>fy</label>
+                                    <input className='halfInput' type='number' step="0.01" ref={fyVal} defaultValue={0} />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='rp-item'>
+                            <div>Image center position</div>
+                            <div className='multiInput'>
+                                <div className='halfText'>
+                                    <label>cx</label>
+                                    <input className='halfInput' type='number' step="0.01" ref={cxVal} defaultValue={0} />
+                                </div>
+                                <div className='halfText'>
+                                    <label>cy</label>
+                                    <input className='halfInput' type='number' step="0.01" ref={cyVal} defaultValue={0} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className='uploaded-gallery'>
                     <div className='ug-header'>Uploaded Images</div>
                     {images.map((image, index) => (
